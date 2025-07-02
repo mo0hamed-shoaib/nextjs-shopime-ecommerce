ebfc241b47a2e66dffff07dbacba05187f173ff4
Mohamed Gamal <mohamed.g.shoaib@gmail.com>
2025-07-03 01:55:52 +0300

    feat: setup Next.js configuration with Facebook & Google image domains

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [
+      'fakestoreapi.in',
+      'storage.googleapis.com',
+      'lh3.googleusercontent.com',
+      'platform-lookaside.fbsbx.com', // Facebook profile images
+    ],
+  },
 };
 
 export default nextConfig;
